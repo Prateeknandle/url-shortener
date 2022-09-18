394661b1877f33f55a74157e07f6ba64c701b6ba
Prateeknandle <prateeknandle@gmail.com>
2022-09-18 14:00:59 +0530

    changed storing logic from map to file

--- handlers/urlshort.go
+++ handlers/urlshort.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -10,28 +11,40 @@ import (
 	"github.com/teris-io/shortid"
 )
 
-var store = make(map[string]string)
+//var store = make(map[string]string)
 
 type long struct {
 	Long_url string `json:"long_url"`
 }
 
-var c = false
+func Split(r rune) bool {
+	return r == ';' || r == '='
+}
+
+var urlstr = "http://localhost:3000/"
 
 func Urlshortner(w http.ResponseWriter, r *http.Request) {
 
+	var c = false
 	var url long
-	urlstr := "http://localhost:3000/"
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&url)
 	if err != nil {
 		log.Fatalf("failed to decode r.Body into long")
 	}
 	// logic for same short url if  same url is passed
-	for key := range store {
-		if url.Long_url == store[key] {
+	// for key := range store {
+	// 	if url.Long_url == store[key] {
+	// 		c = true
+	// 		log.Println("Generated Short Link : ", urlstr+key)
+	// 	}
+	// }
+	data, err := ioutil.ReadFile("data.txt")
+	set := strings.FieldsFunc(string(data), Split)
+	for k, v := range set {
+		if v == url.Long_url {
 			c = true
-			log.Println("Generated Short Link : ", urlstr+key)
+			log.Println("Generated Short Link : ", urlstr+set[k-1])
 		}
 	}
 
@@ -44,7 +57,7 @@ func Urlshortner(w http.ResponseWriter, r *http.Request) {
 		// The package `shortid` enables the generation of short,
 		// fully unique, non-sequential and by default URL friendly Ids
 		// at a rate of hundreds of thousand per second. It guarantees uniqueness
-		//during the time period until 2050!
+		// during the time period until 2050!
 		sid, err := shortid.New(1, shortid.DefaultABC, 2342)
 		if err != nil {
 			log.Fatalln("Error while generating code")
@@ -54,16 +67,15 @@ func Urlshortner(w http.ResponseWriter, r *http.Request) {
 			log.Fatalf("error while generating unique number")
 		}
 		log.Println("Generated Short Link : ", urlstr+urlCode)
-		store[urlCode] = url.Long_url //storing the original url corresponding to the short url
+		//store[urlCode] = url.Long_url //storing the original url corresponding to the short url
 		// storing the new long_url corresponding to its shortened url
-		if _, err = f.WriteString(urlstr + urlCode + " = " + url.Long_url + ";\n"); err != nil {
+		if _, err = f.WriteString(urlCode + "=" + url.Long_url + ";"); err != nil {
 			panic(err)
 		}
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-
 }
 
 func Redirecturl(w http.ResponseWriter, r *http.Request) {
@@ -71,8 +83,14 @@ func Redirecturl(w http.ResponseWriter, r *http.Request) {
 	var actual_url string
 	path := r.URL.Path
 	p := strings.Split(path, "/")
-	actual_url = store[p[1]]
-	//log.Println("actual_url : ", actual_url)
+	//actual_url = store[p[1]]
+	data, _ := ioutil.ReadFile("data.txt")
+	set := strings.FieldsFunc(string(data), Split)
+	for k, v := range set {
+		if v == p[1] {
+			actual_url = set[k+1]
+		}
+	}
 	http.Redirect(w, r, actual_url, http.StatusPermanentRedirect) // redirect to original url corresponding to the generated ID
 
 }
