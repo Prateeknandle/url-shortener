7f236460b84b487b75f7c6b0ce88ae9982fa3112
Prateeknandle <prateeknandle@gmail.com>
2022-09-18 11:15:56 +0530

    creating file and storing data

--- handlers/urlshort.go
+++ handlers/urlshort.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/teris-io/shortid"
@@ -35,6 +36,11 @@ func Urlshortner(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !c {
+		// cheching for file, if not found, it will generate a new file named "data.txt"
+		f, err := os.OpenFile("data.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
 		// The package `shortid` enables the generation of short,
 		// fully unique, non-sequential and by default URL friendly Ids
 		// at a rate of hundreds of thousand per second. It guarantees uniqueness
@@ -49,6 +55,13 @@ func Urlshortner(w http.ResponseWriter, r *http.Request) {
 		}
 		log.Println("Generated Short Link : ", urlstr+urlCode)
 		store[urlCode] = url.Long_url //storing the original url corresponding to the short url
+		// storing the new long_url corresponding to its shortened url
+		if _, err = f.WriteString(urlstr + urlCode + " = " + url.Long_url + ";\n"); err != nil {
+			panic(err)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 }
@@ -59,7 +72,7 @@ func Redirecturl(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	p := strings.Split(path, "/")
 	actual_url = store[p[1]]
-	log.Println("actual_url : ", actual_url)
+	//log.Println("actual_url : ", actual_url)
 	http.Redirect(w, r, actual_url, http.StatusPermanentRedirect) // redirect to original url corresponding to the generated ID
 
 }
